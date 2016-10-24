f88fbb170d0bcaa678345af0ded576ce3d215cf3
soichit <soichit@uw.edu>
2016-10-23 21:09:17 -0700

    added popup

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -13,17 +13,32 @@ export class AppComponent {
   stars3: number = 0;
   stars4: number = 0;
   stars5: number = 0;
+  showPopup: boolean = false;
 
   constructor(private httpService: HttpService) {}
 
   onSubmit() {
     console.log("Easy to use: " + this.stars1);
    
-    this.httpService.sendData( {"user": {username: "Soichi", email: "soichi@gmail.com"},
-      "ratings": { rating1: this.stars1, rating2: this.stars2, rating3: this.stars3, rating4: this.stars4, rating5: this.stars5} })
-        .subscribe(
-          data => console.log(data),
-          error => console.log(error)
-        );
+    this.httpService.sendData({ "user": { username: "Soichi", email: "test@gmail.com" },
+      "ratings": { rating1: this.stars1, rating2: this.stars2, rating3: this.stars3, rating4: this.stars4, rating5: this.stars5} 
+    }).subscribe(
+      data => console.log(data),
+      error => console.log(error)
+    );
+    this.reset();
+    this.showPopup = true;
   }
+
+  reset() {
+    this.stars1 = 0;
+    this.stars2 = 0
+    this.stars3 = 0;
+    this.stars4 = 0;
+    this.stars5 = 0;
+  }
+
+    dismissDialog() {
+      this.showPopup = false;
+    }
 }
